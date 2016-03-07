c6364806b3aa9bf1d92f65e9fd690579e721e1cb
Oliver Neukum <oneukum@suse.com>
2016-03-07 11:31:10 +0100

    usbnet: cleanup after bind() in probe()
    [ Upstream commit 1666984c8625b3db19a9abc298931d35ab7bc64b ]
    
    In case bind() works, but a later error forces bailing
    in probe() in error cases work and a timer may be scheduled.
    They must be killed. This fixes an error case related to
    the double free reported in
    http://www.spinics.net/lists/netdev/msg367669.html
    and needs to go on top of Linus' fix to cdc-ncm.
    
    Signed-off-by: Oliver Neukum <ONeukum@suse.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Sasha Levin <sasha.levin@oracle.com>
    
    Conflicts:
    	drivers/net/usb/usbnet.c

--- drivers/net/usb/usbnet.c
+++ drivers/net/usb/usbnet.c
@@ -2279,7 +2279,7 @@ free_urb:
 unbind:
 	if (info->unbind)
 		info->unbind (dev, udev);
-free_netdevice:
+out1:
 	/* subdrivers must undo all they did in bind() if they
 	 * fail it, but we may fail later and a deferred kevent
 	 * may trigger an error resubmitting itself and, worse,
