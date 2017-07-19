e0cd29fcc48ac4907ec92b05b1456b4918bdda55
Luca Stefani <luca020400@lineageos.org>
2017-07-19 23:33:03 +0200

    ANDROID: binder: Fix code style
    * Restore style from ed9e9028cfb2789262843dbb7191db3cf859eec5 upstream
    
    Change-Id: I99d3bd1250a0ce935e2e15f17ff556993100172c

--- drivers/staging/android/binder.c
+++ drivers/staging/android/binder.c
@@ -2196,12 +2196,9 @@ static void binder_transaction(struct binder_proc *proc,
 	tcomplete->type = BINDER_WORK_TRANSACTION_COMPLETE;
 	list_add_tail(&tcomplete->entry, &thread->todo);
 	if (target_wait) {
-		if (reply || !(t->flags & TF_ONE_WAY)) {
+		if (reply || !(t->flags & TF_ONE_WAY))
 			wake_up_interruptible_sync(target_wait);
-		}
-		else {
-			wake_up_interruptible(target_wait);
-		}
+		else
 	}
 	return;
 
